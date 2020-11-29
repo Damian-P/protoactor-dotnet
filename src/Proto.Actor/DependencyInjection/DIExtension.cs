760dbbdcba75e571a90345d5dd61d6ff6bfdcc49
Damian.P <dampiecho@gmail.com>
2020-11-29 18:12:24 +0100

    Merge Proto.Actor.Extensions

--- src/Proto.Actor/DependencyInjection/DIExtension.cs
+++ src/Proto.Actor/DependencyInjection/DIExtension.cs
@@ -1,5 +1,7 @@
 using System;
 using JetBrains.Annotations;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Proto.Extensions;
 
 namespace Proto.DependencyInjection
@@ -7,23 +9,60 @@ namespace Proto.DependencyInjection
     [PublicAPI]
     public class DIExtension : IActorSystemExtension<DIExtension>
     {
-        public DIExtension(IDependencyResolver resolver)
+        public DIExtension(ActorSystem system, IDependencyResolver resolver)
         {
             Resolver = resolver;
+            system.Extensions.Register(this);
         }
         public IDependencyResolver Resolver { get; }
     }
 
-    public static class Extensions 
+    public static class Extensions
     {
-        public static ActorSystem WithServiceProvider(this ActorSystem actorSystem, IServiceProvider serviceProvider)
+        public static ActorSystem WithServiceProvider(this ActorSystem actorSystem, IServiceProvider serviceProvider, Action<ActorPropsRegistry>? configureProps)
         {
-            var dependencyResolver = new DependencyResolver(serviceProvider);
-            var diExtension = new DIExtension(dependencyResolver);
-            actorSystem.Extensions.Register(diExtension);
+            var actorPropsRegistry = new ActorPropsRegistry();
+            configureProps?.Invoke(actorPropsRegistry);
+            var dependencyResolver = new DependencyResolver(serviceProvider, actorSystem, actorPropsRegistry);
+            _ = new DIExtension(actorSystem, dependencyResolver);
             return actorSystem;
         }
-        
+
+        /// <summary>
+        /// Register ProtoActor in the service collection
+        /// </summary>
+        /// <param name="services">The service collection</param>
+        /// <param name="registerAction">Actor factory props registration</param>
+        public static IServiceCollection AddProtoActor(this IServiceCollection services,
+            Action<ActorPropsRegistry>? configureProps = null)
+        {
+            var registry = new ActorPropsRegistry();
+            configureProps?.Invoke(registry);
+            services.AddSingleton(registry);
+            services.AddSingleton<IDependencyResolver, DependencyResolver>();
+            services.AddSingleton<DIExtension>();
+            services.AddSingleton(new ActorSystem());
+            return services;
+        }
+
+        /// <summary>
+        /// Register ProtoActor in the service collection
+        /// </summary>
+        /// <param name="services">The service collection</param>
+        /// <param name="registerAction">Actor factory props registration</param>
+        public static IServiceCollection AddProtoActor(this IServiceCollection services,
+            ActorSystemConfig config,
+            Action<ActorPropsRegistry>? configureProps = null)
+        {
+            var registry = new ActorPropsRegistry();
+            configureProps?.Invoke(registry);
+            services.AddSingleton(registry);
+            services.AddSingleton<IDependencyResolver, DependencyResolver>();
+            services.AddSingleton<DIExtension>();
+            services.AddSingleton(new ActorSystem(config));
+            return services;
+        }
+
         public static IDependencyResolver DI(this ActorSystem system) => system.Extensions.Get<DIExtension>().Resolver;
     }
 }
--- src/Proto.Actor/DependencyInjection/DependencyResolver.cs
+++ src/Proto.Actor/DependencyInjection/DependencyResolver.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Proto.DependencyInjection
@@ -6,14 +7,90 @@ namespace Proto.DependencyInjection
     public class DependencyResolver : IDependencyResolver
     {
         private readonly IServiceProvider _services;
+        private readonly ActorSystem _actorSystem;
+        private readonly ActorPropsRegistry _actorPropsRegistry;
 
-        public DependencyResolver(IServiceProvider services)
+        public DependencyResolver(IServiceProvider services, ActorSystem actorSystem, ActorPropsRegistry actorPropsRegistry)
         {
             _services = services;
+            _actorSystem = actorSystem;
+            _actorPropsRegistry = actorPropsRegistry;
         }
 
-        public Props PropsFor<TActor>() where TActor : IActor => Props.FromProducer(() => _services.GetService<TActor>()!);
+        public Props PropsFor<TActor>() where TActor : IActor
+        {
+            if (!_actorPropsRegistry.RegisteredProps.TryGetValue(typeof(TActor), out var props))
+            {
+                props = x => x;
+            }
+            return props(Props.FromProducer(() => ActivatorUtilities.GetServiceOrCreateInstance<TActor>(_services)));
+        }
 
-        public Props PropsFor(Type actorType) => Props.FromProducer(() => (IActor)_services.GetService(actorType)!);
+        public Props PropsFor(Type actorType)
+        {
+            if (!typeof(IActor).GetTypeInfo().IsAssignableFrom(actorType))
+            {
+                throw new InvalidOperationException($"Type {actorType.FullName} must implement {typeof(IActor).FullName}");
+            }
+            if (!_actorPropsRegistry.RegisteredProps.TryGetValue(actorType, out var props))
+            {
+                props = x => x;
+            }
+            return props(Props.FromProducer(() => (IActor)ActivatorUtilities.GetServiceOrCreateInstance(_services, actorType)));
+        }
+
+        public PID RegisterActor<T>(T actor, string? id = null, string? address = null, IContext? parent = null)
+            where T : IActor
+        {
+            id ??= typeof(T).FullName;
+            return GetActor(id, address, parent, () => CreateActor<T>(id, parent, () => new Props().WithProducer(() => actor)));
+        }
+
+        public PID GetActor(string id, string? address = null, IContext? parent = null)
+        {
+            return GetActor(id, address, parent, () => throw new InvalidOperationException($"Actor not created {id}"));
+        }
+
+        public PID GetActor<T>(string? id = null, string? address = null, IContext? parent = null)
+            where T : IActor
+        {
+            id ??= typeof(T).FullName;
+            return GetActor(id, address, parent, () => CreateActor<T>(id, parent, () => new Props().WithProducer(() => ActivatorUtilities.GetServiceOrCreateInstance<T>(_services))));
+        }
+
+        private PID GetActor(string id, string? address, IContext? parent, Func<PID> create)
+        {
+            address ??= "nonhost";
+
+            var pidId = id;
+            if (parent != null)
+            {
+                pidId = $"{parent.Self!.Id}/{id}";
+            }
+
+            var pid = PID.FromAddress(address, pidId);
+            var reff = _actorSystem.ProcessRegistry.Get(pid);
+            if (reff is DeadLetterProcess)
+            {
+                pid = create();
+            }
+            return pid;
+        }
+
+        private PID CreateActor<T>(string id, IContext? parent, Func<Props> producer)
+            where T : IActor
+        {
+            if (!_actorPropsRegistry.RegisteredProps.TryGetValue(typeof(T), out var props))
+            {
+                props = x => x;
+            }
+
+            var props2 = props(producer());
+            if (parent == null)
+            {
+                return _actorSystem.Root.SpawnNamed(props2, id);
+            }
+            return parent.SpawnNamed(props2, id);
+        }
     }
 }
